ec86b66cc28b09434d3b439a8c3b485648998f85
zorro-amarillo <ya6ka0@gmail.com>
2022-02-24 03:48:06 +0300

    feat: add function reverse

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 module.exports = function reverse (n) {
-  
+    let numAbsolute = Math.abs(n);
+    let numToStr = [numAbsolute].toString();
+    let result = numToStr.split('').reverse().join('');
+    return result;
 }
